1342775d6edebf9d1026c540026afeec6ee90cac
bobmek <revbobmek@gmail.com>
2016-08-16 15:52:47 -0400

    put in more real detail
    Made it look more like what the real rat data files would look like for
    the excel file, and started integrating changes to read in excel file
    into python

--- aoc calculations.py
+++ aoc calculations.py
@@ -2,7 +2,7 @@
 """
 
 """
-#import openpyxl
+import openpyxl
 import numpy as np
 import numpy.polynomial.polynomial as poly
 import scipy
@@ -10,10 +10,16 @@ from scipy import integrate
 
 
 
-#wb = openpyxl.load_workbook('example.xlsx')
-#s1=wb.get_sheet_by_name('Sheet1')
+wb = openpyxl.load_workbook('example.xlsx')
+s1=wb.get_sheet_by_name('Sheet1')
+
+
 
 time=[0, 10, 20, 30, 40, 50, 60, 80, 100, 120, 150, 180, 240]
+
+#for m in range(0, s1.max_column):
+    
+    
 
 rat1=[458,	380,	317,	316,	251,	262,	241,	239,	281,	296,	334,	342, 298]
 
@@ -26,6 +32,7 @@ d_coefs_roots=np.roots(d_coefs[ : : -1])
 
 for m in range(0,3):
     print '%d'%(m)
+    #rat_'%d'=m %(m)
     if 0 < d_coefs_roots[m] < 150:
         tnadir=d_coefs_roots[m]
         break
@@ -33,13 +40,11 @@ for m in range(0,3):
 BGnadir=np.polyval(coefs[ : : -1], tnadir)
 BGdrop=coefs[0]-BGnadir
 
-tnadj=93.668
-#tnadj=BGdrop*0.374672
+#tnadj=93.668
+tnadj=BGdrop*0.374672
 
 AUC0totnadj=integrate.quad(ins_func,0,tnadj)
 AUCtnadjto240=integrate.quad(ins_func,tnadj,240)
 
 AOC0totnadj=tnadj*coefs[0]-AUC0totnadj[0]
 AOCtnadjto240=(240-tnadj)*coefs[0]-AUCtnadjto240[0]
-
-
