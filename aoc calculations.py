d7ed97b6c3c3fbf0b3e777630d36c11cce56002f
bobmek <revbobmek@gmail.com>
2016-08-18 16:13:12 -0400

    Finally Read in Excel file!
    read excel file in to the code and am now calculating mean of a single
    TA. Not sure if this needs to be scaled up or not. Will get this piece
    working and revisit.

--- aoc calculations.py
+++ aoc calculations.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-
-"""
 import openpyxl
 import numpy as np
 import numpy.polynomial.polynomial as poly
@@ -13,38 +9,44 @@ from scipy import integrate
 wb = openpyxl.load_workbook('example.xlsx')
 s1=wb.get_sheet_by_name('Sheet1')
 
+rats_RawData=np.array([[cell.value for cell in col] for col in s1['K1':'W7']])
+
+#np.average()
+numRats=len(rats_RawData)
 
 
-time=[0, 10, 20, 30, 40, 50, 60, 80, 100, 120, 150, 180, 240]
+time=rats_RawData[0]
+rats=rats_RawData[1:numRats]
+
+mean_rat=np.average(rats,axis=0)
+
+# find the mean of the time series data
+
+#for m in range 
 
 #for m in range(0, s1.max_column):
-    
-    
+#rat1=[458,	380,	317,	316,	251,	262,	241,	239,	281,	296,	334,	342, 298]
 
-rat1=[458,	380,	317,	316,	251,	262,	241,	239,	281,	296,	334,	342, 298]
+mean_coefs=poly.polyfit(time,mean_rat,4)
+mean_fit=poly.polyval(time, mean_coefs)
+ins_func=lambda x: mean_coefs[0]+mean_coefs[1]*x+ mean_coefs[2]*x**2+mean_coefs[3]*x**3+mean_coefs[4]*x**4
 
-coefs=poly.polyfit(time,rat1,4)
-ffit=poly.polyval(time, coefs)
-ins_func=lambda x: coefs[0]+coefs[1]*x+ coefs[2]*x**2+coefs[3]*x**3+coefs[4]*x**4
-
-d_coefs=poly.polyder(coefs)
+d_coefs=poly.polyder(mean_coefs)
 d_coefs_roots=np.roots(d_coefs[ : : -1])
 
 for m in range(0,3):
     print '%d'%(m)
-    #rat_'%d'=m %(m)
     if 0 < d_coefs_roots[m] < 150:
-        tnadir=d_coefs_roots[m]
+        tnadir=np.real(d_coefs_roots[m])
         break
 
-BGnadir=np.polyval(coefs[ : : -1], tnadir)
-BGdrop=coefs[0]-BGnadir
+BGnadir=np.polyval(mean_coefs[ : : -1], tnadir)
+BGdrop=mean_coefs[0]-BGnadir
 
-#tnadj=93.668
 tnadj=BGdrop*0.374672
 
 AUC0totnadj=integrate.quad(ins_func,0,tnadj)
 AUCtnadjto240=integrate.quad(ins_func,tnadj,240)
 
-AOC0totnadj=tnadj*coefs[0]-AUC0totnadj[0]
-AOCtnadjto240=(240-tnadj)*coefs[0]-AUCtnadjto240[0]
+AOC0totnadj=tnadj*mean_coefs[0]-AUC0totnadj[0]
+AOCtnadjto240=(240-tnadj)*mean_coefs[0]-AUCtnadjto240[0]
