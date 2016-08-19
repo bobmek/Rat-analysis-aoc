4ac2fb7e74f822bd8b0a922eee00077dbf4b4906
bobmek <revbobmek@gmail.com>
2016-08-19 14:38:04 -0400

    It now does the calculations!

--- aoc calculations.py
+++ aoc calculations.py
@@ -20,6 +20,10 @@ rats=rats_RawData[1:numRats]
 ind_coefs=np.zeros((numRats-1,5))
 ind_d_coefs=np.zeros((numRats-1,4))
 ind_d_roots=np.zeros((numRats-1,3), dtype=np.complex_)
+ind_AUC0totnadj=np.zeros((numRats-1,2))
+ind_AUCtnadjto240=np.zeros((numRats-1,2))
+ind_AOC0totnadj=np.zeros((numRats-1,1))
+ind_AOCtnadjto240=np.zeros((numRats-1,1))
 #ind_ffit=np.zeros((numRats-1, len(time)))
 #ind_ins_func=np.empty((numRats-1,1))
 
@@ -31,28 +35,26 @@ mean_fit=poly.polyval(time, mean_coefs)
 mean_ins_func=lambda x: mean_coefs[0]+mean_coefs[1]*x+ mean_coefs[2]*x**2+mean_coefs[3]*x**3+mean_coefs[4]*x**4
 
 for m in range (0, numRats-1):
-    print '%d'%(m)
+    #print '%d'%(m)
     ind_coefs[m]=poly.polyfit(time,rats[m],4)
     ind_d_coefs[m]=poly.polyder(ind_coefs[m])
     dummycoefs=ind_d_coefs[m]
     dummycoefs=dummycoefs[ : : -1]
     ind_d_roots[m]=np.roots(dummycoefs)
-    #dummycoefs=np.zeros((4,1))
-    #ind_ffit[m]=poly.polyval(time, ind_coefs[m])
-    # ind_ins_func[m]=lambda x: ind_coefs[0]+ind_coefs[1]*x+ ind_coefs[2]*x**2+ind_coefs[3]*x**3+ind_coefs[4]*x**4
-
-
-
+    #ind_ins_func[m]=lambda x: ind_coefs[m,0]+ind_coefs[m,1]*x+ ind_coefs[m,2]*x**2+ind_coefs[m,3]*x**3+ind_coefs[m,4]*x**4
 
 
 d_coefs=poly.polyder(mean_coefs)
 d_coefs_roots=np.roots(d_coefs[ : : -1])
 
+#finding the mean nadir
 for m in range(0,3):
     print '%d'%(m)
     if 0 < d_coefs_roots[m] < 150:
         tnadir=np.real(d_coefs_roots[m])
         break
+
+#finding the individual nadirs
 
 mean_BGnadir=np.polyval(mean_coefs[ : : -1], tnadir)
 mean_BGdrop=mean_coefs[0]-mean_BGnadir
@@ -64,3 +66,11 @@ mean_AUCtnadjto240=integrate.quad(mean_ins_func,tnadj,240)
 
 mean_AOC0totnadj=tnadj*mean_coefs[0]-mean_AUC0totnadj
 mean_AOCtnadjto240=(240-tnadj)*mean_coefs[0]-mean_AUCtnadjto240
+
+for m in range (0, numRats-1):
+    ind_ins_func=lambda x: ind_coefs[m,0]+ind_coefs[m,1]*x+ ind_coefs[m,2]*x**2+ind_coefs[m,3]*x**3+ind_coefs[m,4]*x**4
+    ind_AUC0totnadj[m]=integrate.quad(ind_ins_func,0,tnadj)
+    ind_AUCtnadjto240[m]=integrate.quad(ind_ins_func,tnadj,240)
+    ind_AOC0totnadj[m]=tnadj*ind_coefs[m,0]-ind_AUC0totnadj[m,0]
+    ind_AOCtnadjto240[m]=(240-tnadj)*ind_coefs[m,0]-ind_AUCtnadjto240[m,0]
+
