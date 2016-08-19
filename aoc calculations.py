52a13bb182bd7df1dfd4b783bb16f61342769cec
bobmek <revbobmek@gmail.com>
2016-08-19 12:50:37 -0400

    adding in individual rats analyses
    Laying some of the groundwork to be able to do individual rats at the
    same time as the mean.

--- aoc calculations.py
+++ aoc calculations.py
@@ -17,19 +17,33 @@ numRats=len(rats_RawData)
 
 time=rats_RawData[0]
 rats=rats_RawData[1:numRats]
+ind_coefs=np.zeros((numRats-1,5))
+ind_d_coefs=np.zeros((numRats-1,4))
+ind_d_roots=np.zeros((numRats-1,3), dtype=np.complex_)
+#ind_ffit=np.zeros((numRats-1, len(time)))
+#ind_ins_func=np.empty((numRats-1,1))
 
 mean_rat=np.average(rats,axis=0)
 
-# find the mean of the time series data
-
-#for m in range 
-
-#for m in range(0, s1.max_column):
-#rat1=[458,	380,	317,	316,	251,	262,	241,	239,	281,	296,	334,	342, 298]
 
 mean_coefs=poly.polyfit(time,mean_rat,4)
 mean_fit=poly.polyval(time, mean_coefs)
-ins_func=lambda x: mean_coefs[0]+mean_coefs[1]*x+ mean_coefs[2]*x**2+mean_coefs[3]*x**3+mean_coefs[4]*x**4
+mean_ins_func=lambda x: mean_coefs[0]+mean_coefs[1]*x+ mean_coefs[2]*x**2+mean_coefs[3]*x**3+mean_coefs[4]*x**4
+
+for m in range (0, numRats-1):
+    print '%d'%(m)
+    ind_coefs[m]=poly.polyfit(time,rats[m],4)
+    ind_d_coefs[m]=poly.polyder(ind_coefs[m])
+    dummycoefs=ind_d_coefs[m]
+    dummycoefs=dummycoefs[ : : -1]
+    ind_d_roots[m]=np.roots(dummycoefs)
+    #dummycoefs=np.zeros((4,1))
+    #ind_ffit[m]=poly.polyval(time, ind_coefs[m])
+    # ind_ins_func[m]=lambda x: ind_coefs[0]+ind_coefs[1]*x+ ind_coefs[2]*x**2+ind_coefs[3]*x**3+ind_coefs[4]*x**4
+
+
+
+
 
 d_coefs=poly.polyder(mean_coefs)
 d_coefs_roots=np.roots(d_coefs[ : : -1])
@@ -40,13 +54,13 @@ for m in range(0,3):
         tnadir=np.real(d_coefs_roots[m])
         break
 
-BGnadir=np.polyval(mean_coefs[ : : -1], tnadir)
-BGdrop=mean_coefs[0]-BGnadir
+mean_BGnadir=np.polyval(mean_coefs[ : : -1], tnadir)
+mean_BGdrop=mean_coefs[0]-mean_BGnadir
 
-tnadj=BGdrop*0.374672
+tnadj=mean_BGdrop*0.374672
 
-AUC0totnadj=integrate.quad(ins_func,0,tnadj)
-AUCtnadjto240=integrate.quad(ins_func,tnadj,240)
+mean_AUC0totnadj=integrate.quad(mean_ins_func,0,tnadj)
+mean_AUCtnadjto240=integrate.quad(mean_ins_func,tnadj,240)
 
-AOC0totnadj=tnadj*mean_coefs[0]-AUC0totnadj[0]
-AOCtnadjto240=(240-tnadj)*mean_coefs[0]-AUCtnadjto240[0]
+mean_AOC0totnadj=tnadj*mean_coefs[0]-mean_AUC0totnadj
+mean_AOCtnadjto240=(240-tnadj)*mean_coefs[0]-mean_AUCtnadjto240
