ac95ba77bf6720248ae6dbdd3bbc51f0c73c547e
bobmek <revbobmek@gmail.com>
2016-09-30 17:15:48 -0400

    another analog analyzed
    Re-analyzed T-0706 from 092916
    
    code seems to hold up ok

--- aoc calculations.py
+++ aoc calculations.py
@@ -12,9 +12,9 @@ s2=wb1.get_sheet_by_name('Sheet2')
 s3=wb1.get_sheet_by_name('Sheet3')
 s4=wb1.get_sheet_by_name('Sheet4')
 analog='T-0737'
-writesheet=s3
+writesheet=s2
 
-rats_RawData=np.array([[cell.value for cell in col] for col in s1['D7':'P11']])
+rats_RawData=np.array([[cell.value for cell in col] for col in s1['D2':'P6']])
 
 #np.average()
 numRats=len(rats_RawData)
